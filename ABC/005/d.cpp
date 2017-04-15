c4a3e81cc8e1da76467cb9edf29c6cd6d25d26b8
625tk <625tkn@gmail.com>
2017-04-16 00:15:09 +0900

    いらない部分消した

--- ABC/005/d.cpp
+++ ABC/005/d.cpp
@@ -50,38 +50,4 @@ int main(void){
   }
 
   return 0;
-
-
-
-
-/* 
-  a.resize(n);
-  for(int &a1 : a)cin >> a1;
-  a.push_back(-100);
-  cin >> m;
-  b.resize(m);
-  for(int &a1 : b)cin >> a1;
-  
-  int i = 0;
-
-  for(const int &be : b){
-    while(a[i] + t < be){
-      i++;
-      if(i >= n){
-        cout << "no" << endl;
-        return 0;
-      }
-    }
-    if(a[i] > be){
-        cout << "no" << endl;
-        return 0;
-    }
-    i++;
-  }
-  cout << "yes" << endl;
-
-
-  return 0;
-*/
 }
-
