259cde097bf3709034e3f9d245c4c259acbed110
ichigo-o-re <ichigo.o.re.t.k@gmail.com>
2019-07-20 20:39:54 +0900

    a

---
+++ ABC/133/a.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<pii,int> piii;
+typedef pair<ll,ll> pll;
+
+#define reps(i,f,n) for(int i = int(f); i <= int(n); i++)
+#define rep(i,n) reps(i,0,int(n)-1)
+#define rrep(i,n) for(int i = n-1; i >= 0; i--)
+
+#define all(x) (x).begin(),(x).end()
+#define X first
+#define Y second
+
+#define sz size()
+#define eb emplace_back
+#define pb push_back
+
+int main(void){
+  int a, b, n;
+  cin >> n >> a >> b;
+  cout << min(n*a, b) << endl;
+  return 0;
+}
